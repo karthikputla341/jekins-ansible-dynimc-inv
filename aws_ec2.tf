c6289ed525c7cf84c4ff85c016b8326bdd75be6d
karthikputla341 <139814211+karthikputla341@users.noreply.github.com>
2023-10-09 23:06:25 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
+key_name = "karthikputla"
 tags = {
 Name =  "tomcatservers"
 }
